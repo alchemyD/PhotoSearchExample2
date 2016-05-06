ecbac218cde5d586924a534f2c4bfd95082d573e
Dan Dorner <ddorner@ralmac-mms2802.oriental.com>
2016-05-06 14:16:21 -0500

    Started App add bridge file

---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Dan Dorner on 5/6/16.
+//  Copyright © 2016 Daniel Dorner. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "Photo Search Example/AFNetworking/AFNetworking.h"
+
+
+#endif /* bridge_h */
